10b3c55f50977dc5522199421ecde0e422b73dab
Dr3ll <stumpy126@web.de>
2012-10-27 11:40:57 +0200

    kjvfdioubcsaw

---
+++ WarTornLands/WarTornLands/Input/Key.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace WarTornLands
+{
+    class Key : InputKey
+    {
+        private int _held;
+        private bool _value;
+        private Keys _key;
+        private Buttons _button;
+        private int _mode;
+
+        public void SetActivator(Keys key)
+        {
+            _key = key;
+        }
+        public void SetActivator(Buttons button)
+        {
+            _button = button;
+        }
+
+        public bool Value
+        {
+            get { return _value; }
+        }
+
+        public void Update(int mode)
+        {
+            if (mode == 0)
+            {
+                KeyboardState state = Keyboard.GetState();
+
+                _value = state.IsKeyDown(_key);
+            }
+
+            if (mode == 1)
+            {
+                _value = GamePad.GetState(PlayerIndex.One).IsButtonDown(_button);
+            }
+        }
+    }
+}
---
+++ WarTornLands/WarTornLands/PlayerClasses/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WarTornLands.PlayerClasses
+{
+    class Player : DrawableGameComponent
+    {
+        Vector2 _position = Vector2.Zero;
+        Texture2D _texture;
+
+        public Player(Game game) : base(game) { }
+
+        public override void Update(GameTime gameTime)
+        {
+            Inputmanager input = (Game as Game1).input;
+
+            _position += input.Move;
+
+            base.Update(gameTime);
+        }
+
+        public void LoadContent(ContentManager cm)
+        {
+            _texture = cm.Load<Texture2D>("player");
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            (Game as Game1).spriteBatch.Begin();
+            (Game as Game1).spriteBatch.Draw(_texture, _position - new Vector2(_texture.Height) * .5f, Color.White);
+            (Game as Game1).spriteBatch.End();
+
+ 	        base.Draw(gameTime);
+        }
+    }
+}
