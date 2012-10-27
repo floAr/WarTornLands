29b575e9f1b2bba1af35daa6f899d7186967b99e
Dr3ll <stumpy126@web.de>
2012-10-27 15:39:49 +0200

    CollDec

---
+++ WarTornLands/WarTornLands/PlayerClasses/CollisionDetector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WarTornLands.PlayerClasses
+{
+    static class CollisionDetector
+    {
+        private static Level _level;
+
+        public static void Setup(Level level)
+        {
+            _level = level;
+        }
+
+        public static Vector2 GetPosition(Vector2 start, Vector2 toGoal)
+        {
+            float range = toGoal.Length();
+            toGoal.Normalize();
+
+            if(range == 0)
+            {
+                return start;
+            }
+
+            Vector2 curGoal = start + toGoal * range;
+
+            while(!_level.IsPixelAccessible((curGoal)))
+            {
+                if (range > 1)
+                {
+                    range -= 1;
+                }
+                else
+                {
+                    return start;
+                }
+
+                curGoal = start + toGoal * range;
+            }
+
+            return curGoal;
+        }
+
+    }
+}
