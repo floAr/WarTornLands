8902de7559a45c67b3998bd70a7c6e3d633fe68e
Daerst <david.kuri@st.ovgu.de>
2012-10-27 17:04:13 +0200

    Suo / Player

--- WarTornLands/WarTornLands/PlayerClasses/Player.cs
+++ WarTornLands/WarTornLands/PlayerClasses/Player.cs
@@ -14,15 +14,36 @@ namespace WarTornLands.PlayerClasses
         float _radius = 0;
         Texture2D _texture;
 
+        const float _maxHitTime = 1.0f;
+        float _hitTimer;
+
         public Player(Game game) : base(game) { }
 
         public override void Update(GameTime gameTime)
         {
             InputManager input = (Game as Game1).input;
 
+            // Move
             _position = CollisionDetector.GetPosition(_position, input.Move * _radius);
 
+            // Attack
+            _hitTimer -= gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+            if (_hitTimer < 0) _hitTimer = 0.0f;
+            if (input.Hit)
+            {
+                Hit(gameTime);
+            }
+
             base.Update(gameTime);
+        }
+
+        public void Hit(GameTime gameTime)
+        {
+            if (_hitTimer == 0.0f)
+            {
+                _hitTimer = _maxHitTime;
+                // TODO SMACK!
+            }
         }
 
         public void LoadContent(ContentManager cm)
