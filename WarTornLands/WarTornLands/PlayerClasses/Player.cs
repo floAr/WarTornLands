5ac67fbfb3c223b27d061ad972fe00c220b3b095
Dr3ll <stumpy126@web.de>
2012-10-27 15:38:42 +0200

    collDec

--- WarTornLands/WarTornLands/PlayerClasses/Player.cs
+++ WarTornLands/WarTornLands/PlayerClasses/Player.cs
@@ -11,7 +11,7 @@ namespace WarTornLands.PlayerClasses
     class Player : DrawableGameComponent
     {
         Vector2 _position = Vector2.Zero;
-        float _radius = 16;
+        float _radius = 0;
         Texture2D _texture;
 
         public Player(Game game) : base(game) { }
