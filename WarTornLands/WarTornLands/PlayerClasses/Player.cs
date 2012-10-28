2d2dfe0664c9de210342d9c0753140fa07d12273
Daerst <david.kuri@st.ovgu.de>
2012-10-28 17:28:59 +0100

    Player trinkt Potion auch aus, und wird nicht nur geheilt.

--- WarTornLands/WarTornLands/PlayerClasses/Player.cs
+++ WarTornLands/WarTornLands/PlayerClasses/Player.cs
@@ -215,6 +215,7 @@ namespace WarTornLands.PlayerClasses
             if (_potionCount > 0)
             {
                 _health += 3 + new System.Random().Next(8);
+                _potionCount--;
             }
         }
     }
