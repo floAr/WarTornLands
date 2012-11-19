85cd6bf2ad0f6f1d779dc399aa6226719f0f73d1
floAr <floar@gmx.de>
2012-11-19 21:01:07 +0100

    added comments

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Interact/_IInteractModule.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Interact/_IInteractModule.cs
@@ -8,10 +8,18 @@ namespace WarTornLandsRefurbished.Entities.Modules.Interact
 {
     interface IInteractModule
     {
+        /// <summary>
+        /// Additional information to pass to interaction
+        /// </summary>
         public struct InteractInformation
         {
         }
-
+        /// <summary>
+        /// Called when an interaction is invoked
+        /// </summary>
+        /// <param name="invoker">Entity which started the interaction</param>
+        /// <param name="target">Entity that has been interacted with</param>
+        /// <param name="information">additional interaction information</param>
         public void Interact(Entity invoker, Entity target, InteractInformation information);
     }
 }
