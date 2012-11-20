d8ba4d350dfb2124505479b2aa1be09b5f419498
unknown <flip@UMMON.(none)>
2012-11-20 17:23:54 +0100

    Input Module

---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Think/Parts/SwingHitAbility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarTornLands.Counter;
+using WarTornLands.Entities;
+
+namespace WarTornLandsRefurbished.Entities.Modules.Think.Parts
+{
+    class SwingHitAbility
+    {
+        /// <summary>
+        /// Gets or sets the duration of a swing.
+        /// </summary>
+        /// <value>
+        /// The duration in milliseconds.
+        /// </value>
+        public int Duration { get; set; }
+        /// <summary>
+        /// Gets or sets the expanse angle of the swing cone.
+        /// </summary>
+        /// <value>
+        /// The angle in radians.
+        /// </value>
+        public float Angle { get; set; }
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="SwingHitAbility" /> is executing a hit.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if executing; otherwise, <c>false</c>.
+        /// </value>
+        public bool Active
+        {
+            get { return _cm.GetPercentage(_cJump) != 0; }
+        }
+
+        private CounterManager _cm;
+        private Entity _owner;
+
+        // Counters
+        private readonly string _cJump = "SwingHitCounter";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SwingHitAbility" /> class.
+        /// </summary>
+        /// <param name="owner">The owner.</param>
+        /// <param name="duration">The duration of a swing.</param>
+        /// <param name="angle">The angle of the swing cone.</param>
+        public SwingHitAbility(Entity owner, int duration = 700, float angle = 20)
+        {
+            Duration = duration;
+            Angle = angle;
+
+            _cm = owner.CM;
+            _cm.Bang += new EventHandler<BangEventArgs>(OnBang);
+
+            _owner = owner;
+        }
+
+        #region Subscribed events
+
+        private void OnBang(object sender, BangEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
--- WarTornLandsRefurbished/WarTornLandsRefurbished/World/Area.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/World/Area.cs
@@ -2,56 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using WarTornLands.World.Layers;
-using Microsoft.Xna.Framework;
 
 namespace WarTornLandsRefurbished.World
 {
     public class Area
     {
-        private Game _game;
-        private LinkedList<Layer> _layers;
-        
-        /// <summary>
-        /// Create an area objects. The area is not automatically added to the
-        /// Game's Component list. Use Add() or Remove() to do this.
-        /// </summary>
-        /// <param name="game"></param>
-        public Area(Game game)
-        {
-            _game = game;
-            _layers = new LinkedList<Layer>();
-        }
-
-        /// <summary>
-        /// Add a Layer to the Area. A layer can contain either Tiles or Entities.
-        /// </summary>
-        /// <param name="layer"></param>
-        public void AddLayer(Layer layer)
-        {
-            _layers.AddLast(layer);
-        }
-
-        /// <summary>
-        /// Adds all layers of the area to the Game's Component list.
-        /// </summary>
-        public void Add()
-        {
-            foreach (Layer layer in _layers)
-            {
-                _game.Components.Add(layer);
-            }
-        }
-
-        /// <summary>
-        /// Removes all layers of the area from the Game's Component list.
-        /// </summary>
-        public void Remove()
-        {
-            foreach (Layer layer in _layers)
-            {
-                _game.Components.Remove(layer);
-            }
-        }
+        public void Remove() { }
+        public void Add() { }
     }
 }
