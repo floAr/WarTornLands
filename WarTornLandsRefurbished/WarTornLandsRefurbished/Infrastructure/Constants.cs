1f6dfb68b6eabb37b1abcb2c7c91d80f195c9e9c
Dr3ll <stumpy126@web.de>
2012-11-26 19:28:52 +0100

    constants

---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Infrastructure/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarTornLands.Infrastructure
+{
+    class Constants
+    {
+        public readonly static int TileSize = 64;
+    }
+}
