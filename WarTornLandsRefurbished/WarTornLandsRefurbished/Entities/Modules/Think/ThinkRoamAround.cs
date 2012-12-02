c15601a3a2c01175461332ccaddcb09ca3bb377a
Dr3ll <stumpy126@web.de>
2012-12-02 16:34:02 +0100

    They see me Roamin'

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Think/ThinkRoamAround.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Think/ThinkRoamAround.cs
@@ -7,31 +7,84 @@ using Microsoft.Xna.Framework;
 using WarTornLands.Entities;
 using WarTornLands.Infrastructure;
 using WarTornLands.Entities.Modules;
+using WarTornLands.Entities.Modules.Think.Parts;
+using WarTornLands.PlayerClasses;
 
 namespace WarTornLandsRefurbished.Entities.Modules.Think
 {
     public class ThinkRoamAround : BaseModule, IThinkModule
     {
-        private Vector2 _anchor;
-        private Vector2 _target;
-        private float _radius;
+        public float SightRange { get; set; }
 
+        private enum RoamState 
+        {
+            Idle = 0,
+            Aggro = 1
+        }
+
+        private GoToPosition _goTo;
+
+        private Vector2 _anchor;
+        private float _radius;
+        private Random _rand;
+        private RoamState _state;
 
         public ThinkRoamAround(Entity owner, Vector2 anchor, float radius)
             : base()
         {
+            _goTo = new GoToPosition();
+            _rand = new Random();
             _anchor = anchor;
-            _target = anchor;
             _radius = radius;
         }
 
-        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        public void Update(GameTime gameTime)
         {
-            //if(_owner.Position is close to _target)
-            //  wähle neues target im radius um den anchor
-            //  lasse Pfad berechnen
-            //
-            //Gehe einen Schritt in Richtung des nächsten targets
+            switch (_state)
+            {
+                case RoamState.Idle:
+                    IdleActions(gameTime);
+                    break;
+                case RoamState.Aggro:
+                    AggroActions(gameTime);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CheckForTarget()
+        {
+            if ((Player.Instance.Position - _owner.Position).LengthSquared() < SightRange * SightRange)
+            {
+                _state = RoamState.Aggro;
+            }
+            else
+            {
+                _state = RoamState.Idle;
+            }
+        }
+
+        private void IdleActions(GameTime gameTime)
+        {
+            if (!_goTo.Active)
+            {
+                _goTo.TargetPosition = CreateTargetPosition();
+            }
+        }
+
+        private void AggroActions(GameTime gameTime)
+        {
+
+        }
+
+        private Vector2 CreateTargetPosition()
+        {
+            Vector2 res = new Vector2((float)_rand.NextDouble(), (float)_rand.NextDouble());
+            res.Normalize();
+            res *= _radius;
+
+            return res;
         }
     }
 }
