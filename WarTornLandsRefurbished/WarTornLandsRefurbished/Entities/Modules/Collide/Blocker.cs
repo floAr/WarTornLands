13ec10a07d8f60e4397be2a77e9d3a4a91058ccf
floAr <floar@gmx.de>
2012-12-03 19:12:49 +0100

    block module added

---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Collide/Blocker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarTornLands.Entities.Modules.Collide
+{
+    class Blocker:BaseModule, ICollideModule
+    {
+        public bool OnCollide(CollideInformation info)
+        {
+            return false;
+        }
+    }
+}
