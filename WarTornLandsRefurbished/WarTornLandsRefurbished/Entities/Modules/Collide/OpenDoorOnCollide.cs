a1c5f1abd7edfefe8f1aea4955819cbda862ffa7
floAr <floar@gmx.de>
2012-12-03 19:39:27 +0100

    door behaviour expanded

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Collide/OpenDoorOnCollide.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Collide/OpenDoorOnCollide.cs
@@ -7,11 +7,40 @@ namespace WarTornLands.Entities.Modules.Collide
 {
     class OpenDoorOnCollide : BaseModule, ICollideModule
     {
+        private bool _locked;
+        private string _id;
+
+        public OpenDoorOnCollide()
+        {
+            _locked=false;
+        }
+        public OpenDoorOnCollide(String id)
+        {
+            _id = id;
+            _locked = true;
+        }
         public bool OnCollide(CollideInformation info)
         {
             // TODO open only if player has key!
-            _owner.IsDead = true;
-            return true;
+            if (_locked)
+            {
+                if (Game1.Instance.Player.Inventory.HasKey(_id))
+                {
+                    _owner.IsDead = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            else
+            {
+                _owner.IsDead = true;
+                return true;
+            }
         }
     }
 }
