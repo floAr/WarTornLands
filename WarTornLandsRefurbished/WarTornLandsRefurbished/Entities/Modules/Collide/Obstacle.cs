bc0d8d1b667954ef91fcb7092bf102d1746f1505
floAr <floar@gmx.de>
2012-12-03 19:13:55 +0100

    blocker added

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Collide/Obstacle.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Collide/Obstacle.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace WarTornLands.Entities.Modules.Collide
 {
-    class Blocker:BaseModule, ICollideModule
+    class Obstacle:BaseModule, ICollideModule
     {
         public bool OnCollide(CollideInformation info)
         {
