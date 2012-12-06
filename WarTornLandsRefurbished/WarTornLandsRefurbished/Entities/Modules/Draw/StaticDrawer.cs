ee89dfd68af35aa8c2129be02719fe20e1b8a805
Daerst <david.kuri@st.ovgu.de>
2012-12-06 12:19:54 +0100

    Fixed DialogBox positioning, added blink stuff to StaticDrawer for Gruselute

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Draw/StaticDrawer.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Draw/StaticDrawer.cs
@@ -25,6 +25,13 @@ namespace WarTornLands.Entities.Modules.Draw
         public Texture2D Texture { get { return _tex; } set { _tex = value; _size = new Vector2(_tex.Width, _tex.Height); } }
         public bool IsLight { get { return _isLight; } set { _isLight = value; } }
 
+        // Whether the last draw call was invulnerable
+        private bool _invulnerable = false;
+        // Counts the time in the current "blink" state when invulnerable (ms)
+        private float _blinkTime = 0;
+        // Total duration of a normal/highlighted blink duration (ms)
+        private float _blinkDuration = 400;
+
         /// <summary>
         /// Draws the specified batch.
         /// </summary>
@@ -43,9 +50,24 @@ namespace WarTornLands.Entities.Modules.Draw
             Vector2 center = Game1.Instance.Camera.Center;
             Rectangle bounds = Game1.Instance.Window.ClientBounds;
 
+            // Invulnerable counter configuration
+            if (_invulnerable != information.Invulnerable)
+            {
+                _invulnerable = information.Invulnerable;
+                if (!_invulnerable)
+                    _blinkTime = 0;
+            }
+
+            // Set color according to invulnerable and counter state
+            Color color;
+            if (_invulnerable && _blinkTime < _blinkDuration / 2.0)
+                color = Color.Red; // Red in the first half of the counter
+            else
+                color = Color.White; // White in the second half or when not invulnerable
+
             batch.Draw(Texture, new Rectangle((int)_loc.X - (int)center.X + (int)Math.Round(bounds.Width / 2.0f),
                 (int)_loc.Y - (int)center.Y + (int)Math.Round(bounds.Height / 2.0f), (int)_size.X, (int)_size.Y),
-                new Rectangle(0, 0, (int)_size.X, (int)_size.Y), Color.White, information.Rotation, _size / 2, SpriteEffects.None, 0.5f);
+                new Rectangle(0, 0, (int)_size.X, (int)_size.Y), color, information.Rotation, _size / 2, SpriteEffects.None, 0.5f);
         }
 
         public Vector2 Size
@@ -56,7 +78,12 @@ namespace WarTornLands.Entities.Modules.Draw
 
         public void Update(GameTime gameTime)
         {
-           
+            if (_invulnerable)
+            {
+                _blinkTime += gameTime.ElapsedGameTime.Milliseconds;
+                while (_blinkTime > _blinkDuration)
+                    _blinkTime -= _blinkDuration;
+            }
         }
 
     }
