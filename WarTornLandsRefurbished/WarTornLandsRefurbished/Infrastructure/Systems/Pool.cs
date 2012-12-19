abe9631e0d86af327b75268380aad05bb0009de2
floAr <floar@gmx.de>
2012-12-19 09:27:57 +0100

    Documented pool. Pool may now use any type of object, as long as it has an empty constructor OR a factory method is provided. If none of this is the case the returned object may be false

--- WarTornLandsRefurbished/WarTornLandsRefurbished/Infrastructure/Systems/Pool.cs
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Infrastructure/Systems/Pool.cs
@@ -11,11 +11,18 @@ namespace WarTornLands.Infrastructure.Systems
 
         private Func<T> _factory = null;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pool{T}" /> class.
+        /// </summary>
         public Pool()
         {
             _deadObjects = new Queue<T>();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pool{T}" /> class.
+        /// </summary>
+        /// <param name="factory">Parameterless factory method, which returns a new instance of type T</param>
         public Pool(Func<T> factory)
         {
             _deadObjects = new Queue<T>();
@@ -23,7 +30,10 @@ namespace WarTornLands.Infrastructure.Systems
         }
        
 
-
+        /// <summary>
+        /// Pull a new object of type T from the pool
+        /// </summary>
+        /// <returns>Reused or new T</returns>
         public T AllocateObject()
         {
             if (_deadObjects.Count > 0)
@@ -39,6 +49,10 @@ namespace WarTornLands.Infrastructure.Systems
             }
         }
 
+        /// <summary>
+        /// Returns object of type T into the pool
+        /// </summary>
+        /// <param name="deadObject">The object which will be thown back into the pool</param>
         public void GiveBackObject(T deadObject)
         {
             _deadObjects.Enqueue(deadObject);
