b1df2d53fdcb61bb3443a4763794668bd2a62aea
floAr <floar@gmx.de>
2013-01-13 11:16:48 +0100

    Added new GameStates Added and implemented SmartStorage.cs Added placeholder for Savegame Struct

---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Infrastructure/Systems/GameState/States/LevelState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarTornLands.World;
+
+namespace WarTornLands.Infrastructure.Systems.GameState.States
+{
+    public class LevelState:BaseGameState
+    {
+        private GameSessionState _session;
+        private Level _level;
+
+        public override void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            _level.Update(gameTime);
+            _session.Update(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+           //Draw logic here
+        }
+    }
+}
---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Infrastructure/Systems/SaveLoad/SmartStorage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Storage;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WarTornLands.Infrastructure.Systems.SaveLoad
+{
+   public class SmartStorage<SaveGameStructure>
+    {
+       StorageDevice device = new StorageDevice();
+       StorageContainer container;
+       private Stream openStorage(int slot)
+       {
+           // Open a storage container.
+           IAsyncResult result = device.BeginOpenContainer("WarTornLands", null, null);
+           // Wait for the WaitHandle to become signaled.
+           result.AsyncWaitHandle.WaitOne();
+           container = device.EndOpenContainer(result);
+           // Close the wait handle.
+           result.AsyncWaitHandle.Close();
+           string filename = "WarTornLands"+slot+".sav";
+
+           // Check to see whether the save exists.
+           if (!container.FileExists(filename))
+           {
+               // If not, dispose of the container and return.
+               container.Dispose();
+               return null;
+           }
+           // Open the file.
+           return container.OpenFile(filename, FileMode.Open);
+       }
+
+       public void Save(int slot, SaveGameStructure data)
+       {
+           Stream stream = openStorage(slot);
+           if (stream == null)
+               throw new Exception("Stream to storage was not found");
+           XmlSerializer serializer = new XmlSerializer(typeof(SaveGameStructure));
+           serializer.Serialize(stream, data);
+           // Close the file.
+           stream.Close();
+           // Dispose the container, to commit changes.
+           container.Dispose();
+       }
+
+
+       public SaveGameStructure Load(int slot)
+       {
+           Stream stream = openStorage(slot);
+           if (stream == null)
+               throw new Exception("Stream to storage was not found");
+           XmlSerializer serializer = new XmlSerializer(typeof(SaveGameStructure));
+           SaveGameStructure data = (SaveGameStructure)serializer.Deserialize(stream);
+           // Close the file.
+           stream.Close();
+           // Dispose the container.
+           container.Dispose();
+           return data;
+       }
+    }
+}
