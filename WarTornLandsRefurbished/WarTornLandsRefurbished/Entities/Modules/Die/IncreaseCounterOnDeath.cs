52e9d04b708709a0ec824bcda7cdffe1b70c0871
floAr <floar@gmx.de>
2013-04-17 16:06:40 +0200

    add IncreaseCounterOnDeath.cs

---
+++ WarTornLandsRefurbished/WarTornLandsRefurbished/Entities/Modules/Die/IncreaseCounterOnDeath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WarTornLands.World;
+
+namespace WarTornLands.Entities.Modules.Die
+{
+    class IncreaseCounterOnDeath:BaseModule,IDieModule
+    {
+        private string _counter;
+        private IDieModule _followUpModule;
+
+        public IncreaseCounterOnDeath(String counterIdentifier,IDieModule dieModule)
+        {
+            _followUpModule = dieModule;
+            _counter = "counter_" + counterIdentifier;
+            GlobalState.RegisterValue<int>(_counter);
+        }
+        public void Die()
+        {
+            int currentValue = GlobalState.GetValue<int>(_counter);
+            GlobalState.SetValue<int>(_counter, currentValue++);
+            _followUpModule.Die();
+        }
+    }
+}
